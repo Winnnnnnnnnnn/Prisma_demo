651717867c114c3c7dae8af61b158e0856868b68
Winnnnnnnnnnn <148460004+Winnnnnnnnnnn@users.noreply.github.com>
2025-09-29 15:23:03 +0700

    điều chỉnh package.json để deloy

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,13 +1,27 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+  INestApplication,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    (this as any).$on('beforeExit', async () => {
+      await app.close();
+    });
   }
 }
